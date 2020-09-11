b8196811e954664eb4e1e1d80c7a41868f26f9aa
shenchucheng@wslUbuntu <shenchucheng@126.com>
2020-09-11 23:45:32 +0800

    leetcode md模板 ipynb 模板

---
+++ alglearn/config.py
@@ -0,0 +1,59 @@
+
+MDSTYLE = '''
+# **{questionId}.**
+
+------
+
+# **中文版**
+## [**{titleZh}**]({url})
+
+**难度：** {difficultyZh}
+**标签：** {topicTagsZh}
+**相似题目：** {similarQuestionsZh}
+
+
+## **题目：**
+
+{contentZh}
+
+
+------
+
+
+# **English**
+## [**{title}**]({url})
+
+**Difficulty: ** {difficulty}
+**Topic Tag：** {topicTags}
+**Similar Questions：** {similarQuestions}
+## **Content**
+
+{content}
+
+
+------
+
+
+- **来源：** 力扣（LeetCode）
+- **链接：** [{titleZh}]({url})
+- 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+
+{'questionId': '216',
+ 'title': 'Combination Sum III',
+ 'titleSlug': 'combination-sum-iii',
+ 'content': '<div>\r\n<p>Find all possible combinations of <i><b>k</b></i> numbers that add up to a number <i><b>n</b></i>, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.</p>\r\n\r\n<p><strong>Note:</strong></p>\r\n\r\n<ul>\r\n\t<li>All numbers will be positive integers.</li>\r\n\t<li>The solution set must not contain duplicate combinations.</li>\r\n</ul>\r\n\r\n<p><strong>Example 1:</strong></p>\r\n\r\n<pre>\r\n<strong>Input:</strong> <i><b>k</b></i> = 3, <i><b>n</b></i> = 7\r\n<strong>Output:</strong> [[1,2,4]]\r\n</pre>\r\n\r\n<p><strong>Example 2:</strong></p>\r\n\r\n<pre>\r\n<strong>Input:</strong> <i><b>k</b></i> = 3, <i><b>n</b></i> = 9\r\n<strong>Output:</strong> [[1,2,6], [1,3,5], [2,3,4]]\r\n</pre>\r\n</div>',
+ 'translatedTitle': '组合总和 III',
+ 'translatedContent': '<p>找出所有相加之和为&nbsp;<em><strong>n</strong> </em>的&nbsp;<strong><em>k&nbsp;</em></strong>个数的组合<strong><em>。</em></strong>组合中只允许含有 1 -&nbsp;9 的正整数，并且每种组合中不存在重复的数字。</p>\n\n<p><strong>说明：</strong></p>\n\n<ul>\n\t<li>所有数字都是正整数。</li>\n\t<li>解集不能包含重复的组合。&nbsp;</li>\n</ul>\n\n<p><strong>示例 1:</strong></p>\n\n<pre><strong>输入:</strong> <em><strong>k</strong></em> = 3, <em><strong>n</strong></em> = 7\n<strong>输出:</strong> [[1,2,4]]\n</pre>\n\n<p><strong>示例 2:</strong></p>\n\n<pre><strong>输入:</strong> <em><strong>k</strong></em> = 3, <em><strong>n</strong></em> = 9\n<strong>输出:</strong> [[1,2,6], [1,3,5], [2,3,4]]\n</pre>\n',
+ 'isPaidOnly': False,
+ 'difficulty': 'Medium',
+ 'similarQuestions': '[{"title": "Combination Sum", "titleSlug": "combination-sum", "difficulty": "Medium", "translatedTitle": "\\u7ec4\\u5408\\u603b\\u548c"}]',
+ 'topicTags': [{'name': 'Array',
+   'slug': 'array',
+   'translatedName': '数组',
+   '__typename': 'TopicTagNode'},
+  {'name': 'Backtracking',
+   'slug': 'backtracking',
+   'translatedName': '回溯算法',
+   '__typename': 'TopicTagNode'}]}
---
+++ alglearn/template.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+# -*- coding:UTF-8 -*-
+# AUTHOR: Chucheng Shen
+# DATE: 2020/09/10 Thu
+
+# DESCRIPTION: the template cell for jupyter notebook
+
+
+import os, json
+import logging
+
+from nbformat.v4 import new_markdown_cell, new_code_cell, new_raw_cell, new_notebook
+from nbformat.v4.nbbase import validate
+
+
+CELLKTYPE = ['code', 'markdown', 'raw']
+CELLVALID = {
+    'cell_type': str,
+    'metadata': dict,
+    'outputs': list,
+    'source': list
+}
+
+
+def cell_valid(k, v ):
+    """验证cell键值对有效性
+    Returns:
+        [type]: 注意：无效时，返回有效数据类型
+    """
+    if k in CELLVALID.keys() and type(v) != CELLVALID[k]:
+        return CELLVALID[k]
+
+
+def cell_template(cell_type, **kwargs):
+    """生成 cell 模板
+
+    Args:
+        cell_type (str): cell类型 code/markdown/raw     
+    """
+    if cell_type not in CELLKTYPE:
+        raise NotImplementedError
+
+    if cell_type == 'code':
+        return new_code_cell(**kwargs)
+        # return {
+        #     'cell_type': 'code',
+        #     'execution_count': None,
+        #     'metadata': {},
+        #     'outputs': [],
+        #     'source': []
+        # }
+
+    elif cell_type == 'markdown':
+        return new_markdown_cell(**kwargs)
+        # return {
+        #     'cell_type': 'markdown',
+        #     'metadata': {},
+        #     'source': []
+        # }
+    
+    elif cell_type == 'raw':
+        return new_raw_cell(**kwargs)
+
+
+def valid(cell):
+    """检查添加至Cells中的元素是否有效
+    """
+    if type(cell) is Cell:
+       return True 
+    else:
+        logging.warning('Element of Cells must be {} not {}'.format(Cell, type(cell)))
+        raise TypeError(Cells, Cell, type(cell))
+
+
+class Cells(list):
+    """Notebook的cells数据，对cells函数参数进行有效性验证
+    """
+    def __init__(self, cells=''):
+        for cell in cells:
+            valid(cell)
+        super().__init__(cells)
+
+    def append(self, cell):
+        valid(cell)
+        super().append(cell)
+
+    def extend(self, cells):
+        for cell in cells:
+            valid(cell)
+        super().extend(cells)
+    
+    def insert(self, index, cell):
+        valid(cell)
+        super().insert(index, cell)
+
+
+class Cell(dict):
+    """Notebook的单个cell数据检验，生成对应cell_type模板
+       模板由.template中加载，cell_type类型应该包含在模板中
+    """
+    def __init__(self, cell_type: str = 'code', **kwargs):
+        cell_init = cell_template(cell_type=cell_type, **kwargs)
+        super().__init__(**cell_init)
+
+    def __setitem__(self, k, v):
+        t = cell_valid(k, v)
+        if t:
+            logging.warning('Type of {} must be {} not {}'.format(k, t, type(v)))
+            raise TypeError(k, t, type(v))
+        super().__setitem__(k, v) 
+
+
+def markdowncell(**kwargs):
+    return Cell(cell_type='markdown', **kwargs)
+
+
+def codecell(**kwargs):
+    return Cell(cell_type='code', **kwargs)
+
+
+def rawcell(**kwargs):
+    return Cell(cell_type='raw', **kwargs)
+
+
+class Notebook:
+    def __init__(self, filename: str, pathDir: str = '.', **kwargs):
+        if not filename.endswith('.ipynb'):
+            filename += '.ipynb'
+        self.filename = filename
+        self.pathDir = os.path.join(pathDir, filename)
+        if 'cells' not in kwargs.keys():
+            kwargs['cells'] = Cells()
+        self.__notebook = new_notebook(**kwargs)
+    
+    @property
+    def cells(self):
+        return self.__notebook['cells']
+
+    def save(self, filename: str = ''):
+        """保存成jupyter notebook
+
+        Args:
+            filename (str, optional): 文件名不存在时默认文件名保存. Defaults to ''.
+        """
+        if not filename:
+            filename = self.filename
+        with open(filename, mode='w') as f:
+            validate(self.__notebook)
+            json.dump(self.__notebook, f)
+    
+    def cell_add(self, cell):
+        self.cells.append(cell)
+    
+    def cell_insert(self, index, cell):
+        self.cells.insert(index, cell)
